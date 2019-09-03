b7c9ace76fa7e26a1abad707daf14bf8ef3246ac
Jerry Trieu <38389565+onetrieuhero@users.noreply.github.com>
2019-09-03 08:11:09 -0700

    Create Solution_Sorting.js

---
+++ MajorityElement/Solution_Sorting.js
@@ -0,0 +1,8 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var majorityElement = function(nums) {
+    nums.sort((a,b)=>{return a-b});
+    return nums[Math.floor(nums.length/2)];
+};
